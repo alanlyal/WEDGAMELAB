4613ff61319b49dd0d8fea1ca179e3fc78b399be
Alex <ahuberde@my.centennialcollege.ca>
2026-02-04 16:40:58 -0600

    ai movement added

--- WebGameLab/Assets/scripts/NPCMovement.cs
+++ WebGameLab/Assets/scripts/NPCMovement.cs
@@ -9,18 +9,25 @@ public class NPCMovement : MonoBehaviour
     [SerializeField, Self] private NavMeshAgent agent;
     [SerializeField] private List<GameObject> wayPoints = new List<GameObject>();
     private Vector3 destination;
-    private void OnValidate()
-    {
-        this.ValidateRefs();
-    }
+    private int index;
+    private void OnValidate() => this.ValidateRefs();
+ 
     void Start()
     {
-        wayPoints = GameObject.FindGameObjectsWithTag("waypoints").ToList();
+        wayPoints = GameObject.FindGameObjectsWithTag("waypoint").ToList();
         if (wayPoints.Count < 0) return;
-        agent.destination = wayPoints[0].transform.position;   
+        agent.destination = destination = wayPoints[index].transform.position;   
     }
     void Update()
     {
         if (wayPoints.Count < 0) return;
+        if (Vector3.Distance(transform.position, destination) < 3f)
+        {
+            index = (index + 1) % wayPoints.Count;
+            destination = wayPoints[index].transform.position;
+            agent.destination = destination;
+        }
+            
+        
     }
 }
